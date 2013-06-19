dea9a5cf1a4d7697c5b1047274ea1ed5408d356f
naoki.nagazumi@ntt-at.co.jp <>
2013-06-19 09:21:39 +0000

    TESTCASE Try1

---
+++ src/main/java/org/nagazumi/demo/chat/ChatDataTest.java
@@ -0,0 +1,15 @@
+package org.nagazumi.demo.chat;
+import junit.framework.*;
+
+public class ChatDataTest extends TestCase {
+
+  public ChatDataTest (String name){
+       super(name);
+  }
+  public void testCheckPostData() {
+    ChatData data = new ChatData();
+    data.setName("NameTest");
+    data.setMesg("MesgTest");
+    assertTrue(data.checkPostData());
+  }
+}
