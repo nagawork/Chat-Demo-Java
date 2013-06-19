4948eb200e7cfb66cf14b59f792e02a8983b6709
naoki.nagazumi@ntt-at.co.jp <>
2013-06-19 09:38:52 +0000

    Test Try2

--- src/test/java/org/nagazumi/demo/chat/ChatDataTest.java
+++ src/test/java/org/nagazumi/demo/chat/ChatDataTest.java
@@ -12,4 +12,31 @@ public class ChatDataTest extends TestCase {
     data.setMesg("MesgTest");
     assertTrue(data.checkPostData());
   }
+
+  public void testCheckPostDataNameNull() {
+    ChatData data = new ChatData();
+    data.setMesg("MesgTest");
+    assertFalse(data.checkPostData());
+  }
+  public void testCheckPostDataMesgNull() {
+    ChatData data = new ChatData();
+    data.setName("NameTest");
+    assertFalse(data.checkPostData());
+  }
+
+  public void testCheckPostDataNameBlank() {
+    ChatData data = new ChatData();
+    data.setName("");
+    data.setMesg("MesgTest");
+    assertFalse(data.checkPostData());
+  }
+  public void testCheckPostDataMesgBlank() {
+    ChatData data = new ChatData();
+    data.setName("NameTest");
+    data.setMesg("");
+    assertFalse(data.checkPostData());
+  }
+
 }
+
+
